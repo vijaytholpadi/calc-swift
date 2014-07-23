8f929a42ce9ac133cf79c91dab14c18fc5c3340d
Vijay Tholpadi <vijay@thegeekprojekt.com>
2014-07-23 09:17:15 +0530

    1) Adding UILabel for operator field with IBOutlet 2) Implementing clearTapped, plusTapped and minusTapped

--- testCalc/testCalc/ViewController.swift
+++ testCalc/testCalc/ViewController.swift
@@ -11,18 +11,19 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var answerField: UILabel
+    @IBOutlet var operatorField: UILabel
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func buttonTapped(theButton: UIButton) {
         println(theButton.titleLabel.text)
         println("button tapped!")
@@ -39,14 +40,21 @@ class ViewController: UIViewController {
     
     @IBAction func clearTapped(theButton: UIButton){
         println(theButton.titleLabel.text)
+        answerField.text = "0"
     }
     
     @IBAction func plusTapped(theButton: UIButton) {
         println(theButton.titleLabel.text)
+        if operatorField.text == ""{
+            operatorField.text = "+"
+        }
     }
     
     @IBAction func minusTapped(theButton: UIButton) {
         println(theButton.titleLabel.text)
+        if operatorField.text == ""{
+            operatorField.text = "-"
+        }
     }
     
     @IBAction func multiplyTapped(theButton: UIButton) {
